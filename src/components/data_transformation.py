706b290bb3454bf5708ca5c9a0397919f1c07027
Akash Gaikwad <akash.gaikwad9945@gmail.com>
2024-11-19 22:52:07 +0530

    data transformation component is added

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,95 +1,105 @@
-import sys
-import os
-import pandas as pd
-import numpy as np
+from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import RobustScaler, FunctionTransformer
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.impute import KNNImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-
-from src.constant import *
+import numpy as np
+import pandas as pd
+import os
+import sys
 from src.exception import CustomException
 from src.logger import logging
 from src.utils.main_utils import MainUtils
 from dataclasses import dataclass
 
+TARGET_COLUMN = 'Target'  
 
 @dataclass
 class DataTransformationConfig:
-    artifact_dir = os.path.join(artifact_folder)
-    transformed_train_file_path = os.path.join(artifact_dir,'train.npy')
-    transformed_test_file_path = os.path.join(artifact_dir,'test.npy')
-    transformed_object_file_path = os.path.join(artifact_dir,'preprocessor.pkl')
+    artifact_dir: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+    transformed_object_file_path: str
 
+    def __init__(self, artifact_dir):
+        self.artifact_dir = artifact_dir
+        self.transformed_train_file_path = os.path.join(artifact_dir, 'train.npy')
+        self.transformed_test_file_path = os.path.join(artifact_dir, 'test.npy')
+        self.transformed_object_file_path = os.path.join(artifact_dir, 'preprocessor.pkl')
 
 class DataTransformation:
-    def __init__(self,feature_store_file_path):
+    def __init__(self, feature_store_file_path, artifact_folder):
         self.feature_store_file_path = feature_store_file_path
-
-        self.data_transformation_config = DataTransformationConfig()
-
+        self.data_transformation_config = DataTransformationConfig(artifact_folder)
         self.utils = MainUtils()
 
     @staticmethod
-    def get_data(feature_store_file_path: str) ->pd.DataFrame:
-
+    def get_data(feature_store_file_path: str) -> pd.DataFrame:
         try:
-
             data = pd.read_csv(feature_store_file_path)
-
             data.rename(columns={"Good/Bad": TARGET_COLUMN}, inplace=True)
-
             return data
         except Exception as e:
-            raise CustomException(e,sys)
-        
-    def get_data_transformer_object(self):
+            raise CustomException(e, sys)
 
+    def get_data_transformer_object(self, categorical_cols, numeric_cols):
         try:
-
-            imputer_step = ('imputer',SimpleImputer(strategy='constant', fill_value=0))
-            scaler_step = ('scaler',RobustScaler())
+            imputer_step = ('imputer', KNNImputer(n_neighbors=5))
+            scaler_step = ('scaler', StandardScaler())
 
             preprocessor = Pipeline(
-                steps=[
-                    imputer_step,
-                    scaler_step
-                ]
+                steps=[imputer_step, scaler_step]
             )
-
             return preprocessor
         except Exception as e:
-            raise CustomException(e,sys)
-    
-    def initiate_data_transformation(self):
+            raise CustomException(e, sys)
 
-        logging.info("Entered initiate data transformation method of data transfomration class")
+    def initiate_data_transformation(self):
+        logging.info("Starting data transformation process.")
 
         try:
+            # Load data
             dataframe = self.get_data(feature_store_file_path=self.feature_store_file_path)
 
-            X=dataframe.drop(columns= TARGET_COLUMN)
-            y= np.where(dataframe[TARGET_COLUMN]==-1,0,1)
+            # Impute missing values
+            dataframe['Saving accounts'].fillna(dataframe['Saving accounts'].mode()[0], inplace=True)
+            dataframe['Checking account'].fillna(dataframe['Checking account'].mode()[0], inplace=True)
 
-            X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
+            # Label encoding
+            label_encoding_cols = ['Housing', 'Saving accounts', 'Checking account', 'Purpose', 'Age group']
+            label_encoder = LabelEncoder()
+            for col in label_encoding_cols:
+                dataframe[col] = label_encoder.fit_transform(dataframe[col])
 
-            preprocessor = self.get_data_transformer_object()
+            # Separate features and target
+            X = dataframe.drop(columns=TARGET_COLUMN)
+            y = np.where(dataframe[TARGET_COLUMN] == -1, 0, 1)
 
+            # Address class imbalance using SMOTE
+            smote = SMOTE()
+            X_resampled, y_resampled = smote.fit_resample(X, y)
+
+            # Split into train and test sets
+            X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=42)
+
+            # Data scaling
+            numeric_cols = ['Age', 'Credit amount', 'Duration']
+            preprocessor = self.get_data_transformer_object(categorical_cols=[], numeric_cols=numeric_cols)
+
+            # Preprocessing pipeline
             X_train_scaled = preprocessor.fit_transform(X_train)
             X_test_scaled = preprocessor.transform(X_test)
 
+            # Save preprocessor object
             preprocessor_path = self.data_transformation_config.transformed_object_file_path
             os.makedirs(os.path.dirname(preprocessor_path), exist_ok=True)
+            self.utils.save_object(file_path=preprocessor_path, obj=preprocessor)
 
-            self.utils.save_object(file_path= preprocessor_path, obj= preprocessor)
+            # Combine scaled data and target
+            train_arr = np.c_[X_train_scaled, y_train]
+            test_arr = np.c_[X_test_scaled, y_test]
 
-            train_arr = np.c_[X_train_scaled, np.array(y_train)]
-            test_arr = np.c_[X_test_scaled, np.array(y_test)]
+            return train_arr, test_arr, preprocessor_path
 
-            return (train_arr,test_arr,preprocessor_path)
-        
         except Exception as e:
-            raise CustomException(e,sys) from e
-
-    
+            raise CustomException(e, sys)
