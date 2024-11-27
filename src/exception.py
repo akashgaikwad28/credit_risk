f62bf4d4e27308fe1557babf5df296a9f14b67e3
Akash Gaikwad <akash.gaikwad9945@gmail.com>
2024-11-27 19:21:17 +0530

    exception handling

--- src/exception.py
+++ src/exception.py
@@ -1,27 +1,34 @@
 import sys
+import traceback
 
+def error_message_detail(error, error_detail: sys):
+    try:
+        # Extract traceback details from the sys.exc_info() tuple
+        exc_type, exc_value, exc_tb = error_detail.exc_info()
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
+        # Check if the traceback is available
+        if exc_tb is None:
+            # If no traceback is available, simply return the error message
+            return f"Error occurred: {str(error)}"
 
-    file_name=exc_tb.tb_frame.f_code.co_filename
+        # Extract filename, line number, and error message from the traceback
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+        error_message = str(error)
 
-    error_message="Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error)
-    )
-
-    return error_message
-
-
-
-
+        # Format and return the detailed error message
+        return f"Error occurred in python script name [{file_name}] line number [{line_number}] error message [{error_message}]"
+    except Exception as e:
+        # In case something goes wrong, return a generic error message
+        return f"Error occurred while processing the error message: {str(e)}"
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail: sys):
+    def __init__(self, error_message, error_detail: sys):
+        # Call the base class constructor with the error message
         super().__init__(error_message)
-        self.error_message= error_message_detail(
-            error_message,error_detail=error_detail
-        )
-    
+        
+        # Store the detailed error message generated from error_message_detail
+        self.error_message = error_message_detail(error_message, error_detail)
+
     def __str__(self):
         return self.error_message
